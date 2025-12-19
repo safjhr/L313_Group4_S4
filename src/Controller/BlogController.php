78e0212f7375841f4621d74942e365c24f5f4abc
Alphayra <zefinthefly@gmail.com>
2025-12-19 10:47:29 +0100

    ✨ Ajout du tableau des liens sur la page blog et changement de l'origine de blog à la racine.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\LinkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    #[Route('/', name: 'app_blog')]
+    public function index(LinkRepository $linkRepository): Response
     {
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'links' => $linkRepository->findAll(),
         ]);
     }
 }
